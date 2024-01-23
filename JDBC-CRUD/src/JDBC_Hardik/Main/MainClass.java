3a86d107d5cbbf721eefa02d1bb02262dae2c83e
hardh <hardh@TheHardhik>
2024-01-23 11:52:28 +0530

    Commit

--- JDBC-CRUD/src/JDBC_Hardik/Main/MainClass.java
+++ JDBC-CRUD/src/JDBC_Hardik/Main/MainClass.java
@@ -2,7 +2,8 @@ package JDBC_Hardik.Main;
 
 public class MainClass {
 	public static void main(String []args) {
-		System.out.println("Hello World !!");	
+		System.out.println("Hello World !!");
+		System.out.println("Hello World !!");
 	}
     
 }
