e5d22590270dc003e9316c6861d1416137dd8cfc
hardh <hardh@172.20.52.89>
2024-02-07 22:20:40 +0530

    Minor errors fixed

--- JDBC-CRUD/src/JDBC_Hardik/Main/ConnectionClass.java
+++ JDBC-CRUD/src/JDBC_Hardik/Main/ConnectionClass.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionCLass {
+public class ConnectionClass {
 	public  Connection getconnection() throws ClassNotFoundException, SQLException{
 	Connection con = null;
 	Class.forName("com.mysql.cj.jdbc.Driver");
